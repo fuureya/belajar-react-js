55daae5bee56939c3407a528cc65bdbf5a0e35cd
fuureya <agiljibrin009@gmail.com>
2024-04-04 21:25:48 +0800

    selesai component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+
+function Button(props) {
+  const { warna = 'bg-black', children = 'Kosong!' } = props
+  return (
+    <button className={`h-10 px-6 mx-1 font-semibold rounded-md ${props.warna} text-white`} type='submit'>
+      {props.children}
+    </button>
+  )
+}
 
 function App() {
 
   return (
     <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
-      <p className='font-bold'>Hello Dunia</p>
+      <Button warna="bg-red-700">Tombol Merah</Button>
+      <Button warna="bg-blue-300">Tombol Biru</Button>
+      <Button warna="bg-green-500">Tombol Hijau</Button>
+      <Button />
     </div>
   )
 }
