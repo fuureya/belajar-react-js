c2950a6203c73305d38d7b91bfe2ccd39dd04584
fuureya <agiljibrin009@gmail.com>
2024-04-11 08:00:16 +0800

    event handler

--- src/Components/Elements/Button/Index.jsx
+++ src/Components/Elements/Button/Index.jsx
@@ -1,7 +1,9 @@
 const Button = (props) => {
     const { variant = 'bg-black', children = 'Kosong!' } = props
     return (
-        <button className={`h-10 px-6 mx-1 font-semibold rounded-md ${props.variant} text-white`} type='submit'>
+        <button className={`h-10 px-6 mx-1 font-semibold rounded-md ${props.variant} text-white`} type='button' onClick={() => {
+            console.log('anjay')
+        }}>
             {props.children}
         </button>
     )
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,21 +2,21 @@ import CardProducts from "../Components/Fragments/CardProducts";
 
 const wanjay = [
     {
-        id: 1,
+        id: Math.random(),
         title: "Baju Kemeja Flanel",
         price: 10000,
         image: "../public/img/mio.jpg",
         description: "lorem ipsum dolor sit amet consecterur adipisicing elit. nulla euius rem voluptas assumenda dolorum, quaerat a vel aperiam! sunt doloribus quo, minima quibusdam necessitatibus magni facilis in quia delectus quas!"
     },
     {
-        id: 1,
+        id: Math.random(),
         title: "Baju Sekolah",
         price: 10000,
         image: "../public/img/mio2.jpg",
         description: "lorem ipsum dolor sit amet consecterur adipisicing elit. nulla euius rem voluptas assumenda dolorum, quaerat a vel aperiam! sunt doloribus quo, minima quibusdam necessitatibus magni facilis in quia delectus quas!"
     },
     {
-        id: 1,
+        id: Math.random(),
         title: "Baju Tidur",
         price: 10000,
         image: "../public/img/mio.jpg",
@@ -28,17 +28,18 @@ const Products = () => {
     return (
         <div className="flex justify-center">
             {wanjay.map((item) =>
-                <CardProducts>
+                <CardProducts key={item.id}>
                     <CardProducts.Header image={item.image} />
                     <CardProducts.Body title={item.title}>
                         {item.description}
                     </CardProducts.Body>
                     <CardProducts.Footer price={item.price} />
                 </CardProducts>
-            )}
+            )
+            }
 
 
-        </div>
+        </div >
     );
 };
 
